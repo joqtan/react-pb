fde6790ee9489750817eea9e53b466fe0c491b95
joqtan <joqtanmarmol@gmail.com>
2019-12-11 23:52:51 -0600

    JSX

--- src/index.js
+++ src/index.js
@@ -8,7 +8,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello, Platzi Badges!</h1>
+// const element = <h1>Hello, Platzi Badges!</h1>
+
+//const element = React.createElement('h1', {}, 'Hola! soy los children.')
+
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://platzi.com' },
+//   'Ir a platzi'
+// )
+
+//const name = 'Joqtán Alberto'
+
+// const element = React.createElement(
+//     'h1',
+//     {},
+//     `Hola soy ${name}`
+// )
+
+//const element = <h1>Hola, soy {name}</h1>
+
+// const element = React.createElement(
+//   'div',
+//   {},
+//   React.createElement('h1', {}, 'Hola, soy Joqtán'),
+//   React.createElement('p', {}, 'Soy dev frontend')
+// )
+
+const element = (
+  <div>
+    <h1>Hola, soy Joqtán</h1>
+    <p>Soy dev frontend</p>
+  </div>
+)
 
 const container = document.getElementById('app')
 
