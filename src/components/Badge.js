8b4af15ccd81ff2c9410b0a6f110c86d555ab5dd
joqtan <joqtanmarmol@gmail.com>
2019-12-13 21:42:46 -0600

    estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,28 @@
 import React from 'react'
+import './styles/Badge.css'
 import confLogo from '../images/badge-header.svg'
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo"></img>
         </div>
 
-        <div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="" alt="Avatar"></img>
           <h1>
             Joqtán
             <br /> Marmol
           </h1>
         </div>
 
-        <div>
-          <img src="" alt="Avatar"></img>
-          <p>Frontend developer</p>
-          <p>@joqtan_</p>
+        <div className='Badge__section-info'>
+          <h3>Frontend developer</h3>
+          <div>@joqtan_</div>
         </div>
 
-        <div>
+        <div className='Badge__footer'>
           <p>#platziconf</p>
         </div>
       </div>
