9fef7470d3f53ed062937abcfcb5f241c35c2a73
joqtan <joqtanmarmol@gmail.com>
2019-12-17 19:09:05 -0600

    primera pagina

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('app')
 
 ReactDOM.render(
-  <Badge
-    firstName="Joqtán"
-    lastName="Mármol"
-    avatarURL=''
-    jobTitle="Frontend Dev"
-    twitter="joqtan_"    
-  />,
+  <BadgeNew/>,
   container
 )
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles/BadgeNew.css'
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge'
+import header from '../images/badge-header.svg'
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className='BadgeNew__hero'>
+          <img className='img-fluid' src={header} alt='logo'></img>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <Badge firstName='Joqtán' lastName='Mármol' jobTitle='Frontend developer' twitter='joqtan_'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BadgeNew
